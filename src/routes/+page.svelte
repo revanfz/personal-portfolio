243a5047207cc705dc422c0e08cf46f20a16643e
Refzlgfr <revanfauzi81@gmail.com>
2023-02-03 10:16:00 +0700

    Edit homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1 class="grid h-screen place-content-center text-8xl">Portfolio</h1>
-    
+<div class="h-screen">
+    <h1 class="grid h-5/6 place-content-center text-5xl mb-10">Sm:)le</h1>
+    <p class="text-center">
+        &copy Copyright
+        <strong>Raven</strong>
+        2022. Allright Reserved.
+    </p>
+</div>
