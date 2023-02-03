c1433f99710a6dfa30e98386a504aebfd00d5239
Refzlgfr <revanfauzi81@gmail.com>
2023-02-03 10:33:46 +0700

    Edit homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="h-screen">
-    <h1 class="grid h-5/6 place-content-center text-5xl mt-10">Sm:)le</h1>
-    <p class="text-center">
+    <h1 class="grid h-5/6 place-content-center text-5xl pt-20">Sm:)le</h1>
+    <p class="text-center mt-12">
         &copy Copyright
         <strong>Raven</strong>
         2022. Allright Reserved.
