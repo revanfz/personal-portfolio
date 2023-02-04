41cf12ccebe5a296ab51cffe954581b1212b9c69
Refzlgfr <revanfauzi81@gmail.com>
2023-02-04 23:28:29 +0700

    Edit Home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,34 @@
-<div class="h-screen">
-    <h1 class="grid h-5/6 place-content-center text-5xl pt-20">Sm:)le</h1>
-    <p class="text-center mt-12">
+<div class="snap-start flex items-center justify-around bg-teal-200 h-">
+    <div class="greetings w-1/3">
+        <div class="leading-loose pb-2">
+            <h3 class="text-2xl">Hi! 👋 I am</h3>
+            <h2 class="text-5xl">Revan Fauzi Algifari</h2>
+            <h3 class="text-2xl">Tech Enthusiast</h3>
+        </div>
+        <p>
+            Lorem ipsum dolor sit amet. Apa kaji dek baulang apa jalan dek batampuah.
+            Dima bumi dipijak disitu langik dijunjuang. 
+        </p>
+    </div>
+    <img src="profile.png" width="380" alt="Foto Revan Fauzi Algifari">
+</div>
+
+<div class="snap-start flex items-center justify-center h-screen bg-teal-300" id="about">
+    <h1 class="m-auto text-5xl">About Me</h1>
+</div>
+
+<div class="snap-start flex items-center justify-center h-screen bg-yellow-300" id="portfolio">
+    <h1 class="m-auto text-5xl">My Projects</h1>
+</div>
+
+<div class="snap-start flex items-center justify-center h-screen bg-fuchsia-300" id="contacts">
+    <h1 class="m-auto text-5xl">Contact</h1>
+</div>
+
+<footer class="snap-start">
+    <p class="text-center mx-auto p-8">
         &copy Copyright
         <strong>Raven</strong>
         2022. Allright Reserved.
     </p>
-</div>
+</footer>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        grotesk: ['"Space Grotesk"', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
